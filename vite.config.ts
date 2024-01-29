61c490394f6ed7655e241e953f19e993368eb783
kimjaeseong <jsk@rightbrain.co.kr>
2024-01-29 16:07:12 +0900

    path 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/miraeN_function_test/'
+  base: '/miraeN_function_test/',
+  server: {
+    port: 8080,
+    open: "/miraeN_function_test/"
+  }
 })
