004f97e9d46bec35ef2951704e3ebdbcbb8670f5
kimjaeseong <jsk@rightbrain.co.kr>
2024-01-29 15:57:48 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,15 @@ function App() {
     <>
       <h1>미래엔 기능 테스트 (1.29) <a href="https://react-chartjs-2.js.org/" target="_blank" style={{fontSize:'12px'}}>react-chartjs-2 새창열기</a></h1>
       <ul>
-        <li><Link to="/t1">선생님 홈 & 대시보드</Link></li>
-        <li><Link to="/t2">선생님 수업하기</Link></li>
-        <li><Link to="/s1">학생 수업하기</Link></li>
+        <li><Link to="/miraeN_function_test/t1">선생님 홈 & 대시보드</Link></li>
+        <li><Link to="/miraeN_function_test/t2">선생님 수업하기</Link></li>
+        <li><Link to="/miraeN_function_test/s1">학생 수업하기</Link></li>
       </ul>
       <hr />
       <Routes>
-        <Route path="/t1" element={<T1 /> } />
-        <Route path="/t2" element={<T2 /> } />
-        <Route path="/s1" element={<S1 />} />
+        <Route path="/miraeN_function_test/t1" element={<T1 /> } />
+        <Route path="/miraeN_function_test/t2" element={<T2 /> } />
+        <Route path="/miraeN_function_test/s1" element={<S1 />} />
       </Routes>
     </>
   )
