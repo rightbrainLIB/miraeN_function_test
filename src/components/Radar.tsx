3c5ea6d896838e5f35f4b0973eaf547a374648d9
kimjaeseong <jsk@rightbrain.co.kr>
2024-01-30 12:30:43 +0900

    그라디언트 테스트

--- src/components/Radar.tsx
+++ src/components/Radar.tsx
@@ -19,6 +19,43 @@ ChartJS.register(
   Legend
 );
 
+function createRadialGradient3(context:any) {
+    const chartArea = context.chart.chartArea;
+    if (!chartArea) {
+        // This case happens on initial chart load
+        return;
+      }
+    const chartWidth = chartArea.right - chartArea.left;
+    const chartHeight = chartArea.bottom - chartArea.top;
+
+    const width = chartWidth;
+    const height = chartHeight;
+    const centerX = (chartArea.left + chartArea.right) / 2;
+    const centerY = (chartArea.top + chartArea.bottom) / 2;
+
+    const ctx = context.chart.ctx;
+
+    var gradient = ctx.createConicGradient(-1.0472, centerX, centerY);
+
+    // The pattern is 30 degrees of blend between quadrants
+    // 60 degrees of pure color in the quadrant
+    gradient.addColorStop(0, 'rgba(78, 190, 235, .40)'); //blue
+    gradient.addColorStop(.16667, 'rgba(78, 190, 235, .40)'); //blue
+    gradient.addColorStop(0.25, 'rgba(255, 152, 49, .40)'); //orange
+    gradient.addColorStop(0.41667, 'rgba(255, 152, 49, .40)'); //orange
+    gradient.addColorStop(0.5, 'rgba(96, 230, 225, .40)');  // turqoise
+    gradient.addColorStop(0.66667, 'rgba(96, 230, 225, .40)');  // turqoise
+    gradient.addColorStop(0.75, 'rgba(45, 183, 87, .40)'); //green
+    gradient.addColorStop(0.91667, 'rgba(45, 183, 87, .40)'); //green
+    gradient.addColorStop(1, 'rgba(78, 190, 235, .40)'); //blue
+
+    // Set the fill style and draw a rectangle
+    ctx.fillStyle = gradient;
+    ctx.fillRect(chartArea.left, chartArea.top, chartWidth, chartHeight);
+  
+    return gradient;
+  }
+
 const data = {
   labels: ['듣기', '읽기', '말하기', '쓰기', '단어'],
   datasets: [
@@ -32,7 +69,11 @@ const data = {
     {
       label: '2',
       data: [3, 5, 2, 1, 4],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      // backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      backgroundColor: function (context:any) {
+        console.log(context);
+        return createRadialGradient3(context);
+      },
       borderColor: 'rgba(255, 99, 132, 1)',
       borderWidth: 1,
     },
