2b925b85726ef40b30fe02cfcdebdacc172c1493
syk <syk@rightbrain.co.kr>
2024-01-31 14:41:21 +0900

    [수정] 차트 수정

--- src/pages/T1.tsx
+++ src/pages/T1.tsx
@@ -25,16 +25,20 @@ import {
   LinearScale,
   BarElement,
   Title,
+  PointElement,
+  LineElement,
   Tooltip,
   Legend,
 } from "chart.js";
-import { Bar } from "react-chartjs-2";
+import { Bar, Chart } from "react-chartjs-2";
 import annotationPlugin from "chartjs-plugin-annotation";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
   BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend,
@@ -45,6 +49,7 @@ function T1() {
   const duration = 60;
   const [progress, setProgress] = useState(100);
   const chartRef = useRef(null);
+  const chartRef02 = useRef(null);
   const [annotationY, setAnnotationY] = useState(0);
 
   useEffect(() => {
@@ -146,9 +151,119 @@ function T1() {
     ],
   };
 
+  const options02 = {
+    responsive: true,
+    layout: {
+      padding: 20,
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        display: false,
+        grid: {
+          drawBorder: false,
+          display: false,
+        },
+      },
+    },
+  };
+
+  const createPattern = (
+    backgroundColor: string = "#fff",
+    lineColor: string = "#ccc"
+  ) => {
+    const patternCanvas = document.createElement("canvas");
+    const patternCtx = patternCanvas.getContext("2d");
+    if (!patternCtx) return null;
+
+    patternCanvas.width = 7;
+    patternCanvas.height = 5;
+
+    patternCtx.fillStyle = backgroundColor;
+    patternCtx.fillRect(0, 0, patternCanvas.width, patternCanvas.height);
+
+    patternCtx.strokeStyle = lineColor;
+    patternCtx.lineWidth = 1;
+
+    patternCtx.beginPath();
+    patternCtx.moveTo(0, 5);
+    patternCtx.lineTo(7, 0);
+    patternCtx.stroke();
+
+    return patternCtx.createPattern(patternCanvas, "repeat");
+  };
+
+  const pattern = createPattern(
+    "rgba(255, 99, 132, .3)",
+    "rgba(255, 99, 132, .5)"
+  ) as CanvasPattern;
+
+  const labels02 = ["January", "February", "March", "April"];
+  const data02 = {
+    labels: labels02,
+    datasets: [
+      {
+        type: "line" as const,
+        label: "Dataset 1",
+        borderColor: "rgb(255, 99, 132)",
+        borderWidth: 2,
+        fill: false,
+        data: [60, 50, 100, 150],
+      },
+      {
+        type: "bar" as const,
+        label: "Dataset 2",
+        backgroundColor: pattern,
+        borderRadius: 50,
+        borderColor: "rgb(255, 99, 132)",
+        data: [60, 50, 100, 150],
+        borderWidth: 2,
+      },
+      {
+        type: "bar" as const,
+        label: "Dataset 3",
+        backgroundColor: "rgb(53, 162, 235)",
+        data: [60, 50, 100, 150],
+      },
+    ],
+  };
+
   return (
     <>
       <h2>시간표</h2>
+      <table className="info">
+        <tbody>
+          <tr>
+            <th>mui지원</th>
+            <td>O</td>
+          </tr>
+          <tr>
+            <th>가능</th>
+            <td>Timeline, 애니메이션 keyframes, useEffect interval </td>
+          </tr>
+          <tr>
+            <th>확인중</th>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th>불가</th>
+            <td>없음</td>
+          </tr>
+        </tbody>
+      </table>
       <Stack
         sx={{
           display: "flex",
@@ -242,8 +357,50 @@ function T1() {
         </Box>
       </Stack>
       <h2>학습시간 분석</h2>
+      <table className="info">
+        <tbody>
+          <tr>
+            <th>chartjs</th>
+            <td>O</td>
+          </tr>
+          <tr>
+            <th>가능</th>
+            <td>grid, scales y축 안보이게 가능, annotations 라인추가</td>
+          </tr>
+          <tr>
+            <th>확인중</th>
+            <td>가운데 라인 애니메이션 확인 중</td>
+          </tr>
+          <tr>
+            <th>불가</th>
+            <td>없음</td>
+          </tr>
+        </tbody>
+      </table>
       <Stack sx={{ width: "300px", display: "flex", flexDirection: "row" }}>
         <Bar options={options} data={data} ref={chartRef} />
+      </Stack>
+      <table className="info">
+        <tbody>
+          <tr>
+            <th>mui</th>
+            <td>O</td>
+          </tr>
+          <tr>
+            <th>가능</th>
+            <td> mui 컴포넌트 + transition</td>
+          </tr>
+          <tr>
+            <th>확인중</th>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th>불가</th>
+            <td>없음</td>
+          </tr>
+        </tbody>
+      </table>
+      <Stack>
         <Box sx={{ marginLeft: 10 }}>
           <List>
             <ListItem sx={{ padding: 0 }}>
@@ -290,6 +447,30 @@ function T1() {
             </ListItem>
           </List>
         </Box>
+      </Stack>
+      <h2>학습 시간 변화</h2>
+      <table className="info">
+        <tbody>
+          <tr>
+            <th>chartjs</th>
+            <td>O</td>
+          </tr>
+          <tr>
+            <th>가능</th>
+            <td>bar+line 혼합차트</td>
+          </tr>
+          <tr>
+            <th>확인중</th>
+            <td>막대 그래프 내 패턴 확인 중</td>
+          </tr>
+          <tr>
+            <th>불가</th>
+            <td>없음</td>
+          </tr>
+        </tbody>
+      </table>
+      <Stack sx={{ width: "300px", display: "flex", flexDirection: "row" }}>
+        <Chart type="bar" options={options02} data={data02} ref={chartRef02} />
       </Stack>
     </>
   );
