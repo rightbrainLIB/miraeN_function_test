3c1378caf237d425ca1c76640114d86e487e2843
RByunsu <yss@rightbrain.co.kr>
2024-02-01 16:08:25 +0900

    [수정] 학생 수업하기 차트 수정

---
+++ src/components/S1Bar.tsx
@@ -0,0 +1,101 @@
+import {
+  Chart as ChartJS,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import imgStripe from "../img/stripe.jpg"
+import imgSquare from "../img/square.jpg"
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
+import { Button, ButtonGroup } from '@mui/material';
+
+ChartJS.register(
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,  
+);
+
+const stripe = new Image();
+stripe.src = imgStripe;
+const square = new Image();
+square.src = imgSquare;
+
+const barOptions = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: false,
+    },
+  },
+  scales: {
+    x: {
+      border: {
+        display: true,
+        color: '#888'
+      },
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      border: {
+        display: false,
+        dash: [3,4],
+      },
+      grid: {
+        color: '#aaa',
+        tickColor: '#000',
+        tickBorderDash: [2, 3],
+        tickWidth: 0,
+      },
+    },
+  },
+};
+
+const labels = ['주제 추론', '빈칸 추론', '문장 배열', '문단 요약', '밑줄 의미'];
+const barData = {
+  labels,
+  datasets: [
+    {
+      label: '이하나',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 25 })),
+      backgroundColor: stripe.onload = () => {
+        
+        const shape = document.createElement('canvas')
+        const ctx = shape.getContext('2d')
+        if(ctx) {
+          const fillPattern = ctx.createPattern(stripe, 'repeat');
+          if (fillPattern !== null) {
+            return fillPattern;
+          }
+        }
+      },
+      borderRadius: 100,
+    },
+    {
+      label: '학년',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 25 })),
+      backgroundColor: '#ccc',
+      borderRadius: 100,
+    },
+  ],
+};
+
+function S1Bar() {
+  return (
+	<>
+		<Bar data={barData} options={barOptions} />
+		<ButtonGroup  sx={{width : "100%", paddingLeft: "26px", justifyContent: "space-between"}} variant='text' aria-label="loading button group">
+			{
+				labels.map(()=> {
+					return <Button variant='contained' sx={{fontSize: "12px"}}>학습 추천하기</Button>
+				})
+			}
+		</ButtonGroup>
+	</>)
+}
+
+export default S1Bar
--- src/pages/S1.tsx
+++ src/pages/S1.tsx
@@ -1,151 +1,61 @@
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  PointElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import imgs from "../img/stripe.jpg"
-import { Bar } from 'react-chartjs-2';
-import { Bubble } from 'react-chartjs-2';
-import faker from 'faker';
-import { Box, Button, ButtonGroup } from '@mui/material';
+import { Box } from '@mui/material';
+import S1Bubble from '../components/S1Bubble';
+import S1Bar from '../components/S1Bar';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  PointElement,
-  Title,
-  Tooltip,
-  Legend,
-);
-
-
-const stripe = new Image();
-stripe.src = imgs;
-
-const barOptions = {
-  responsive: true,
-  plugins: {
-    title: {
-      display: false,
-    },
-  },
-  scales: {
-    x: {
-      border: {
-        display: true,
-        color: '#888'
-      },
-      grid: {
-        display: false,
-      },
-    },
-    y: {
-      border: {
-        display: false,
-        dash: [3,4],
-      },
-      grid: {
-        color: '#aaa',
-        tickColor: '#000',
-        tickBorderDash: [2, 3],
-        tickWidth: 0,
-      },
-    },
-  },
-};
-
-const labels = ['주제 추론', '빈칸 추론', '문장 배열', '문단 요약', '밑줄 의미'];
-const barData = {
-  labels,
-  datasets: [
-    {
-      label: '이하나',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 25 })),
-      backgroundColor: stripe.onload = () => {
-        
-        const shape = document.createElement('canvas')
-        const ctx = shape.getContext('2d')
-        if(ctx) {
-          const fillPattern = ctx.createPattern(stripe, 'repeat');
-          if (fillPattern !== null) {
-            return fillPattern;
-          }
-        }
-      },
-      borderRadius: 100,
-    },
-    {
-      label: '학년',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 25 })),
-      backgroundColor: '#ccc',
-      borderRadius: 100,
-    },
-  ],
-};
-
-const bubbleOptions = {
-  scales: {
-    y: {
-      beginAtZero: true,
-    },
-  },
-};
-
-const bubbleData = {
-  datasets: [
-    {
-      label: '우리반',
-      data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }),
-        y: faker.datatype.number({ min: -100, max: 100 }),
-        r: faker.datatype.number({ min: 5, max: 20 }),
-      })),
-      backgroundColor: stripe.onload = () => {
-        const shape = document.createElement('canvas')
-        const ctx = shape.getContext('2d')
-        if(ctx) {
-          const fillPattern = ctx.createPattern(stripe, 'repeat');
-          if (fillPattern !== null) {
-            return fillPattern;
-          }
-        }
-      },
-    },
-    {
-      label: '학년',
-      data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }),
-        y: faker.datatype.number({ min: -100, max: 100 }),
-        r: faker.datatype.number({ min: 5, max: 20 }),
-      })),
-      backgroundColor: '#ddd',
-    },
-  ],
-};
 function S1() {
-
   return (
     <>
       <h2>학생 수업하기</h2>
+      <br />
+      <h2>취약개념 분석 - Bar</h2>
+      <table className="info">
+        <tbody>
+          <tr>
+            <th>chartjs 지원</th>
+            <td>O</td>
+          </tr>
+          <tr>
+            <th>가능</th>
+            <td>데이터에 패턴 넣기, x축 grid점선 표시, y축 없애기, 막대 radius</td>
+          </tr>
+          <tr>
+            <th>확인중</th>
+            <td>낮은 값의 데이터의 경우 막대 radius가 제대로 적용되지 않음</td>
+          </tr>
+          <tr>
+            <th>불가</th>
+            <td>프로토파이 형태의 애니메이션 불가</td>
+          </tr>
+        </tbody>
+      </table>
       <Box sx={{width : "600px", height: "300px", marginBottom:"56px"}}>
-        <Bar data={barData} options={barOptions} />
-        <ButtonGroup  sx={{width : "100%", paddingLeft: "26px", justifyContent: "space-between"}} variant='text' aria-label="loading button group">
-          {
-            labels.map(()=> {
-              return <Button variant='contained' sx={{fontSize: "12px"}}>학습 추천하기</Button>
-            })
-          }
-        </ButtonGroup>
+        <S1Bar />
       </Box>
 
-      <Box sx={{width : "600px", height: "300px"}}>
-        <Bubble options={bubbleOptions} data={bubbleData} />
+      <br />
+      <h2>우리반 학습 효울 분석 - Bubble</h2>
+      <table className="info">
+        <tbody>
+          <tr>
+            <th>chartjs 지원</th>
+            <td>O</td>
+          </tr>
+          <tr>
+            <th>가능</th>
+            <td>학생 데이터에 이름표시, 데이터에 패턴 넣기, 범례 라벨 원표시</td>
+          </tr>
+          <tr>
+            <th>확인중</th>
+            <td>범례와 차트 사이 간격 조정가능 여부</td>
+          </tr>
+          <tr>
+            <th>불가</th>
+            <td>프로토파이 형태의 애니메이션(원이 커지는 모션) 불가</td>
+          </tr>
+        </tbody>
+      </table>
+      <Box sx={{width : "600px", height: "500px"}}>
+        <S1Bubble />
       </Box>
     </>
   )
