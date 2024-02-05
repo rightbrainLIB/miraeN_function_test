7e19e4df58a683baf93c20a2c7f1a763ef042f2b
kimjaeseong <jsk@rightbrain.co.kr>
2024-02-05 16:10:25 +0900

    .

--- src/components/Area.tsx
+++ src/components/Area.tsx
@@ -13,6 +13,8 @@ import {
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import faker from 'faker';
+import imgStripe from "../img/stripe.jpg"
+import imgSquare from "../img/square.jpg"
 
 ChartJS.register(
   CategoryScale,
@@ -24,6 +26,11 @@ ChartJS.register(
   Filler,
   Legend
 );
+
+const stripe = new Image();
+stripe.src = imgStripe;
+const square = new Image();
+square.src = imgSquare;
 
 export const options = {
   responsive: true,
@@ -40,6 +47,30 @@ export const options = {
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
+function createDiagonalPattern(color = 'black') {
+  // create a 10x10 px canvas for the pattern's base shape
+  let shape = document.createElement('canvas')
+  shape.width = 10
+  shape.height = 10
+  // get the context for drawing
+  let c = shape.getContext('2d')
+  if (c !== null) {
+    // draw 1st line of the shape 
+    c.strokeStyle = color
+    c.beginPath()
+    c.moveTo(2, 0)
+    c.lineTo(10, 8)
+    c.stroke()
+    // draw 2nd line of the shape 
+    c.beginPath()
+    c.moveTo(0, 8)
+    c.lineTo(2, 10)
+    c.stroke()
+    // create the pattern from the shape
+    return c.createPattern(shape, 'repeat')
+  }
+}
+
 export const data = {
   labels,
   datasets: [
@@ -48,15 +79,23 @@ export const data = {
       label: 'Dataset 2',
       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
       borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      backgroundColor: createDiagonalPattern('gray'),
     },
     {
       fill: true,
       label: 'Dataset 2',
       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
       borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      backgroundColor: createDiagonalPattern('blue'),
     },
+    {
+      fill: true,
+      label: 'Dataset 3',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: createDiagonalPattern('red'),
+    },
+    
   ],
 };
 
